0043746efa4db49606f556ff115834202c62c7fd
whizsavvy <134269430+whizsavvy@users.noreply.github.com>
2025-04-19 18:54:21 +0900

    Update exe.py

--- EvergreenSlideMaker/exe.py
+++ EvergreenSlideMaker/exe.py
@@ -26,8 +26,10 @@ def create_presentation(hymn_list=[]):
     add_blank_slide(prs)
     add_hymn_slide(prs, hymn_list[0])
     add_hymn_slide(prs, hymn_list[1])
-
-    add_card_slide(prs, input_text= '신앙고백', background_color='000000')
+    add_image_slide(prs, pic_dic+'신앙고백.jpg')
+    add_image_slide(prs, pic_dic+'신앙고백_1.jpg')
+    add_image_slide(prs, pic_dic+'신앙고백_2.jpg')
+    # add_card_slide(prs, input_text= '신앙고백', background_color='000000')
     add_blank_slide(prs)
 
 
