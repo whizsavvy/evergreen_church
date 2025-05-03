b06f0085ceccfee25a6d0ac232d645c9b80a9385
whizsavvy <134269430+whizsavvy@users.noreply.github.com>
2025-05-03 18:54:06 +0900

    Update exe.py

--- EvergreenSlideMaker/exe.py
+++ EvergreenSlideMaker/exe.py
@@ -13,7 +13,7 @@ today = datetime.datetime.now().strftime('%Y-%m-%d')
 
 exec(open("EvergreenSlideMaker/setting.py").read())
 
-hymn_list = ['오늘 숨을 쉬는 것 감사', '예수를 나의 구주 삼고', '주님은 나의 힘이요','하나님의 나라 온 땅 흔드네', '하나님이 세상을 사랑하사', '우리 오늘 눈물로', '이 땅의 동과 서 남과 북']
+hymn_list = ['주 하나님 지으신 모든 세계', '주 이름 찬양','선교사', '십자가를 질 수 있나', '변찮는 주님의 사랑과', '내가 매일 기쁘게', '보혈을 지나', '이 땅의 동과 서 남과 북']
 
 def create_presentation(hymn_list=[]):
     prs = Presentation()
@@ -46,30 +46,43 @@ def create_presentation(hymn_list=[]):
     add_blank_slide(prs)
     add_card_slide(prs, input_text= '성가대 찬양')
     add_blank_slide(prs)
-    add_bible_slide(prs, directory, "요한복음", "3:16")
-    add_subtitle_slide(prs, input_text='가장 큰 사랑의 선물 (요 3:16)')
+    add_bible_slide(prs, directory, "에베소서", "5:15", "5:17")
+    add_subtitle_slide(prs, input_text='인생 후반전, 어떻게 살 것인가 (엡 5:15~17)')
     add_blank_slide(prs)
-    add_subtitle_slide(prs, input_text='1. "하나님이 세상을 이처럼 사랑하사" – 조건 없는 사랑의 선언')
-    add_bible_slide(prs, directory, "로마서", "5:8")
-    add_bible_slide(prs, directory, "요한일서", "4:10")
-    add_subtitle_slide(prs, input_text='2. "독생자를 주셨으니" – 생명을 내어준 선물')
-    
-    add_bible_slide(prs, directory, "요한일서", "5:11")
-    add_bible_slide(prs, directory, "로마서", "6:23")
+    add_bible_slide(prs, directory, "에베소서", "5:15")
+    add_bible_slide(prs, directory, "시편", "90:12")
+    add_bible_slide(prs, directory, "고린도전서", "10:31")
+    add_bible_slide(prs, directory, "마태복음", "5:16")
 
-    add_subtitle_slide(prs, input_text='3. "믿는 자마다 멸망하지 않고 영생을 얻게 하려 하심이라" – 모든 사람에게 열려 있는 구원의 길')
-    add_bible_slide(prs, directory, "베드로후서", "3:9")
-    add_bible_slide(prs, directory, "로마서", "10:13")
-    add_bible_slide(prs, directory, "마태복음", "28:19", "28:20")
+    add_bible_slide(prs, directory, "에베소서", "5:16")
+    add_bible_slide(prs, directory, "전도서", "3:1")
+
+    add_bible_slide(prs, directory, "에베소서", "5:16")
+    add_bible_slide(prs, directory, "로마서", "13:11", "13:12")
+    add_bible_slide(prs, directory, "디모데후서", "3:1", "3:2")
+    add_bible_slide(prs, directory, "마태복음", "24:44")
+
+    add_bible_slide(prs, directory, "에베소서", "5:17")
+    add_bible_slide(prs, directory, "요한복음", "15:7")
+    add_bible_slide(prs, directory, "이사야", "40:31")
+    add_bible_slide(prs, directory, "마태복음", "27:5")
+    add_bible_slide(prs, directory, "에베소서", "5:16")
+    add_bible_slide(prs, directory, "요한복음", "13:27")
+    add_bible_slide(prs, directory, "출애굽기", "7:7")
+    add_bible_slide(prs, directory, "사도행전", "20:24")
+    add_bible_slide(prs, directory, "고린도전서", "15:58")
+    add_bible_slide(prs, directory, "마태복음", "5:23", "5:24")
+    add_bible_slide(prs, directory, "골로새서", "3:13")
+    add_bible_slide(prs, directory, "히브리서", "12:14")
   
     # add_hymn_slide(prs, '나는 주를 섬기는 것에 후회가 없습니다')
     # add_image_slide(prs)
-    # add_card_slide(prs, input_text= '성찬')
+    add_card_slide(prs, input_text= '성찬')
     # add_hymn_slide(prs,  '나 같은 죄인 살리신')
-    add_hymn_slide(prs, hymn_list[5])
+    add_hymn_slide(prs, hymn_list[6])
     add_card_slide(prs, input_text= '통성기도', background_color='000000')
     add_card_slide(prs, input_text= '광고')
-    add_hymn_slide(prs, hymn_list[6])
+    add_hymn_slide(prs, hymn_list[7])
     # add_hymn_slide(prs,  '하늘에 계신(주기도문)')
     add_card_slide(prs, input_text= '축도')
 
