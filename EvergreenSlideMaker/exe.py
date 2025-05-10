898d4438628ae011a8e01dfafd82f30645f298d8
whizsavvy <134269430+whizsavvy@users.noreply.github.com>
2025-05-11 02:27:10 +0900

    Update exe.py

--- EvergreenSlideMaker/exe.py
+++ EvergreenSlideMaker/exe.py
@@ -13,7 +13,7 @@ today = datetime.datetime.now().strftime('%Y-%m-%d')
 
 exec(open("EvergreenSlideMaker/setting.py").read())
 
-hymn_list = ['주 하나님 지으신 모든 세계', '주 이름 찬양','선교사', '십자가를 질 수 있나', '변찮는 주님의 사랑과', '내가 매일 기쁘게', '보혈을 지나', '이 땅의 동과 서 남과 북']
+hymn_list = ['그 크신 하나님의 사랑', '높은 산들 흔들리고', '변찮는 주님의 사랑과', '이 땅 위에 오신', '모든 상황 속에서', '이 땅의 동과 서 남과 북']
 
 def create_presentation(hymn_list=[]):
     prs = Presentation()
@@ -25,18 +25,18 @@ def create_presentation(hymn_list=[]):
     add_image_slide(prs, pic_dic+'2025.jpg', text='주일 2부 예배')
     add_blank_slide(prs)
     add_hymn_slide(prs, hymn_list[0])
-    add_hymn_slide(prs, hymn_list[1])
+    
     add_image_slide(prs, pic_dic+'신앙고백.png')
     add_image_slide(prs, pic_dic+'신앙고백_1.png')
     add_image_slide(prs, pic_dic+'신앙고백_2.png')
     # add_card_slide(prs, input_text= '신앙고백', background_color='000000')
     add_blank_slide(prs)
 
-
+    add_hymn_slide(prs, hymn_list[1])
     add_hymn_slide(prs, hymn_list[2])
     add_hymn_slide(prs, hymn_list[3])
-    add_hymn_slide(prs, hymn_list[4])
-    add_hymn_slide(prs, hymn_list[5])
+
+
 
 
 
@@ -46,43 +46,61 @@ def create_presentation(hymn_list=[]):
     add_blank_slide(prs)
     add_card_slide(prs, input_text= '성가대 찬양')
     add_blank_slide(prs)
-    add_bible_slide(prs, directory, "에베소서", "5:15", "5:17")
-    add_subtitle_slide(prs, input_text='인생 후반전, 어떻게 살 것인가 (엡 5:15~17)')
+    add_bible_slide(prs, directory, "마태복음", "6:31", "6:33")
+    add_bible_slide(prs, directory, "히브리서", "11:6")
+    add_subtitle_slide(prs, input_text='믿음은 오늘을 이기게 합니다')
     add_blank_slide(prs)
-    add_bible_slide(prs, directory, "에베소서", "5:15")
-    add_bible_slide(prs, directory, "시편", "90:12")
-    add_bible_slide(prs, directory, "고린도전서", "10:31")
-    add_bible_slide(prs, directory, "마태복음", "5:16")
+    add_subtitle_slide(prs, input_text='1) 믿음은 하나님과의 관계의 시작입니다.')
+    add_bible_slide(prs, directory, "히브리서", "11:6")
+    add_subtitle_slide(prs, input_text='2) 믿음은 구원의 길입니다.')
+    add_bible_slide(prs, directory, "에베소서", "2:8")
+    add_bible_slide(prs, directory, "로마서", "5:1")
 
-    add_bible_slide(prs, directory, "에베소서", "5:16")
-    add_bible_slide(prs, directory, "전도서", "3:1")
+    add_subtitle_slide(prs, input_text='3) 믿음은 신자의 삶의 방식입니다.')
+    add_bible_slide(prs, directory, "로마서", "1:17")
+    add_bible_slide(prs, directory, "갈라디아서", "2:20")
 
-    add_bible_slide(prs, directory, "에베소서", "5:16")
-    add_bible_slide(prs, directory, "로마서", "13:11", "13:12")
-    add_bible_slide(prs, directory, "디모데후서", "3:1", "3:2")
-    add_bible_slide(prs, directory, "마태복음", "24:44")
+    add_subtitle_slide(prs, input_text='4) 믿음은 세상을 이기는 능력입니다.')
+    add_bible_slide(prs, directory, "요한일서", "5:4")
 
-    add_bible_slide(prs, directory, "에베소서", "5:17")
-    add_bible_slide(prs, directory, "요한복음", "15:7")
-    add_bible_slide(prs, directory, "이사야", "40:31")
-    add_bible_slide(prs, directory, "마태복음", "27:5")
-    add_bible_slide(prs, directory, "에베소서", "5:16")
-    add_bible_slide(prs, directory, "요한복음", "15:27")
-    add_bible_slide(prs, directory, "출애굽기", "7:7")
-    add_bible_slide(prs, directory, "사도행전", "20:24")
-    add_bible_slide(prs, directory, "고린도전서", "15:58")
-    add_bible_slide(prs, directory, "마태복음", "5:23", "5:24")
-    add_bible_slide(prs, directory, "골로새서", "3:13")
-    add_bible_slide(prs, directory, "히브리서", "12:14")
+
+    add_subtitle_slide(prs, input_text='1. 믿음은 평안을 줍니다')
+    add_bible_slide(prs, directory, "시편", "23:1", "23:3")
+    add_bible_slide(prs, directory, "요한복음", "14:27")
+    add_bible_slide(prs, directory, "빌립보서", "4:6", "4:7")
+    add_bible_slide(prs, directory, "이사야", "26:3")
+    add_bible_slide(prs, directory, "마가복음", "5:25", "5:34")
+
+
+    add_subtitle_slide(prs, input_text='2. 믿음은 하나님의 인도하심을 경험하게 합니다')
+    add_bible_slide(prs, directory, "시편", "23:4")
+
+    
+    add_bible_slide(prs, directory, "잠언", "3:5", "3:6")
+    add_bible_slide(prs, directory, "시편", "32:8")
+
+    add_subtitle_slide(prs, input_text='3. 믿음은 공급과 채우심의 은혜를 경험하게 합니다')
+    add_bible_slide(prs, directory, "마태복음", "6:31", "6:33")
+
+    
+
+    add_bible_slide(prs, directory, "열왕기상", "17:8", "17:16")
+    add_bible_slide(prs, directory, "빌립보서", "4:19")
+    add_bible_slide(prs, directory, "말라기", "3:10")
+    add_bible_slide(prs, directory, "출애굽기", "19:4")
+
+    add_bible_slide(prs, directory, "요한일서", "5:4")
+
+
   
     # add_hymn_slide(prs, '나는 주를 섬기는 것에 후회가 없습니다')
     # add_image_slide(prs)
-    add_card_slide(prs, input_text= '성찬')
+    # add_card_slide(prs, input_text= '성찬')
     # add_hymn_slide(prs,  '나 같은 죄인 살리신')
-    add_hymn_slide(prs, hymn_list[6])
+    add_hymn_slide(prs, hymn_list[4])
     add_card_slide(prs, input_text= '통성기도', background_color='000000')
     add_card_slide(prs, input_text= '광고')
-    add_hymn_slide(prs, hymn_list[7])
+    add_hymn_slide(prs, hymn_list[5])
     # add_hymn_slide(prs,  '하늘에 계신(주기도문)')
     add_card_slide(prs, input_text= '축도')
 
