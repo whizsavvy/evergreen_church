64eb920931c434afe334b94c777ef4871350eb64
whizsavvy <134269430+whizsavvy@users.noreply.github.com>
2025-12-20 23:29:48 +0900

    Update exe.py

--- EvergreenSlideMaker/exe.py
+++ EvergreenSlideMaker/exe.py
@@ -23,52 +23,51 @@ def create_presentation(hymn_list=[]):
     add_image_slide(prs, pic_dic+'2025.jpg', text='주일 1부 예배')
     add_image_slide(prs, pic_dic+'2025.jpg', text='주일 2부 예배')
     add_blank_slide(prs)
-    add_hymn_slide(prs, hymn_list[0])
-    add_hymn_slide(prs, hymn_list[1])
-    add_hymn_slide(prs, hymn_list[2])
+    # add_hymn_slide(prs, hymn_list[0])
+    # add_hymn_slide(prs, hymn_list[1])
+    # add_hymn_slide(prs, hymn_list[2])
+    add_card_slide(prs, input_text= '성가대 찬양')
+    add_choir_slides_from_file(prs, box_color="203864")
+    
     add_image_slide(prs, pic_dic+'신앙고백.png')
     add_image_slide(prs, pic_dic+'신앙고백_1.png')
     add_image_slide(prs, pic_dic+'신앙고백_2.png')
     # add_card_slide(prs, input_text= '신앙고백', background_color='000000')
     add_blank_slide(prs)
     # add_hymn_slide(prs, hymn_list[1]) 
-    add_hymn_slide(prs, hymn_list[3])
-    add_hymn_slide(prs, hymn_list[4])
-    add_hymn_slide(prs, hymn_list[5])
+    # add_hymn_slide(prs, hymn_list[3])
+    # add_hymn_slide(prs, hymn_list[4])
+    # add_hymn_slide(prs, hymn_list[5])
 
     add_card_slide(prs, input_text= '통성기도', background_color='000000')
     add_card_slide(prs, input_text= '대표기도')
-    add_card_slide(prs, input_text= '성가대 찬양')
-    add_choir_slides_from_file(prs, box_color="203864")
+    
    
     # 제목/본문 (필수)
-    add_bible_slide(prs, directory, "창세기", "47:7", "47:10")
-    add_subtitle_slide(prs, input_text="네 나이가 얼마냐 (창세기 47:7~10)")
+    add_bible_slide(prs, directory, "누가복음", "2:10", "2:11")
+    add_subtitle_slide(prs, input_text="너희를 위하여 구주가 나셨으니 (누가복음 2:10~11)")
     
-    # RED only — 원고 등장 순서
-    add_bible_slide(prs, directory, "창세기", "45:5", "45:8")   # 요셉의 고백
-    add_bible_slide(prs, directory, "창세기", "47:8")           # “네 나이가 얼마냐?”
-    add_bible_slide(prs, directory, "창세기", "47:9")           # “나그네 길의 세월…”
+    # RED only — 원고 등장 순서 그대로
+    add_bible_slide(prs, directory, "누가복음", "2:10")
+    add_bible_slide(prs, directory, "이사야", "41:10")
+    add_bible_slide(prs, directory, "마태복음", "1:23")
+    add_bible_slide(prs, directory, "요한복음", "14:27")
     
-    add_bible_slide(prs, directory, "창세기", "47:7")
-    add_bible_slide(prs, directory, "창세기", "47:10")          # 야곱이 바로를 축복
+    add_bible_slide(prs, directory, "요한복음", "3:16")
+    add_bible_slide(prs, directory, "디도서", "2:11")
+    add_bible_slide(prs, directory, "로마서", "10:13")
     
-    add_bible_slide(prs, directory, "히브리서", "7:7")
-    add_bible_slide(prs, directory, "창세기", "27:29")
-    add_bible_slide(prs, directory, "빌립보서", "3:7", "3:9")
+    add_bible_slide(prs, directory, "누가복음", "2:11")
+    add_bible_slide(prs, directory, "마태복음", "1:21")
+    add_bible_slide(prs, directory, "요한복음", "1:12")
+    add_bible_slide(prs, directory, "사도행전", "4:12")
+    add_bible_slide(prs, directory, "고린도후서", "5:17")
+    add_bible_slide(prs, directory, "요한복음", "10:10")
     
-    add_bible_slide(prs, directory, "고린도전서", "15:10")
-    add_bible_slide(prs, directory, "에베소서", "5:16")
-    add_bible_slide(prs, directory, "시편", "90:10")
-    add_bible_slide(prs, directory, "시편", "90:9")
-
-
-
-    add_hymn_slide(prs, '왜 나만 겪는 고냔이냐고')
-    add_hymn_slide(prs, '내가 어둠속에서')
+    add_bible_slide(prs, directory, "로마서", "15:13")
+    add_bible_slide(prs, directory, "이사야", "9:6")
     # add_card_slide(prs, input_text= '성찬')
     # add_hymn_slide(prs, '나 같은 죄인 살리신')
-
     add_card_slide(prs, input_text= '통성기도', background_color='000000')
     add_card_slide(prs, input_text= '광고')
     # add_hymn_slide(prs, hymn_list[6])
