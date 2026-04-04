cf2155f8fcf1817b8ca06c56f13d24e1cd442c9a
whizsavvy <134269430+whizsavvy@users.noreply.github.com>
2026-04-05 01:54:07 +0900

    Update exe.py

--- EvergreenSlideMaker/exe.py
+++ EvergreenSlideMaker/exe.py
@@ -13,7 +13,10 @@ today = datetime.datetime.now().strftime('%Y-%m-%d')
 
 exec(open("EvergreenSlideMaker/setting.py").read())
 
-hymn_list = ['우리 때문에', '만왕의 왕 주께서', '왕 되신 주께 감사하세', '나의 하나님', '십자가', '예수 나의 산 소망']
+hymn_list = ['그 사랑', '더 원합니다', '주님 큰 영광 받으소서'
+, '주 이름 큰 능력 있도다', '할렐루야 살아계신 주', '무덤에 머물러'
+
+'나 같은 죄인 살리신' , '예수 나의 산 소망']
 
 def create_presentation(hymn_list=[]):
     prs = Presentation()
@@ -26,47 +29,57 @@ def create_presentation(hymn_list=[]):
 
     add_blank_slide(prs)
     add_hymn_slide(prs, hymn_list[0])
-
+    add_hymn_slide(prs, hymn_list[1])
+    add_hymn_slide(prs, hymn_list[2])
+    
 
    
     # add_image_slide(prs, pic_dic+'신앙고백.png')
     add_image_slide(prs, pic_dic+'2026_신앙고백1.JPG')
     add_image_slide(prs, pic_dic+'2026_신앙고백2.JPG')
     # add_card_slide(prs, input_text= '신앙고백', background_color='000000')
-    add_hymn_slide(prs, hymn_list[1])
-    add_hymn_slide(prs, hymn_list[2])
     add_hymn_slide(prs, hymn_list[3])
+    add_hymn_slide(prs, hymn_list[4])
+    add_hymn_slide(prs, hymn_list[5])
     add_blank_slide(prs)
     
 
     # add_card_slide(prs, input_text= '성가대 찬양')
-    add_bible_slide(prs, directory, "시편", "46:1", "46:11")
-    add_subtitle_slide(prs, input_text="하나님께 집중하다 – 가만히 있으라 (시편 46:1~11)")
+    # add_bible_slide(prs, directory, "시편", "46:1", "46:11")
+    # add_subtitle_slide(prs, input_text="하나님께 집중하다 – 가만히 있으라 (시편 46:1~11)")
     
-    add_bible_slide(prs, directory, "시편", "121:4")
-    add_bible_slide(prs, directory, "시편", "46:1")
-    add_bible_slide(prs, directory, "시편", "46:6")
-    add_bible_slide(prs, directory, "시편", "46:7")
-    add_bible_slide(prs, directory, "시편", "46:10")
-    add_bible_slide(prs, directory, "잠언", "3:5")
-    add_bible_slide(prs, directory, "요한복음", "19:30")
-    add_bible_slide(prs, directory, "예레미야", "2:13")
-    add_bible_slide(prs, directory, "시편", "95:2")
-    add_bible_slide(prs, directory, "시편", "95:6")
-    add_bible_slide(prs, directory, "시편", "62:1")
-    add_bible_slide(prs, directory, "갈라디아서", "2:20")
-    add_bible_slide(prs, directory, "시편", "46:10")
-    add_bible_slide(prs, directory, "요한복음", "19:30")
-    add_hymn_slide(prs, hymn_list[4])
+    add_bible_slide(prs, directory, "고린도전서", "15:3", "15:8")
+    add_subtitle_slide(prs, input_text="믿을 것인가, 부인할 것인가 (고린도전서 15:3~8)")
+    
+    add_bible_slide(prs, directory, "고린도전서", "15:3", "15:8")
+    add_bible_slide(prs, directory, "로마서", "6:23")
+    add_bible_slide(prs, directory, "히브리서", "9:22")
+    add_bible_slide(prs, directory, "레위기", "17:11")
+    add_bible_slide(prs, directory, "히브리서", "10:4")
+    add_bible_slide(prs, directory, "로마서", "3:25")
+    add_bible_slide(prs, directory, "요한일서", "2:2")
+    add_bible_slide(prs, directory, "로마서", "5:8")
+    add_bible_slide(prs, directory, "로마서", "4:25")
+    add_bible_slide(prs, directory, "고린도전서", "15:54", "15:55")
+    add_bible_slide(prs, directory, "히브리서", "2:14")
+    add_bible_slide(prs, directory, "로마서", "1:4")
+    add_bible_slide(prs, directory, "고린도전서", "15:20")
+    add_bible_slide(prs, directory, "요한복음", "14:19")
+    add_bible_slide(prs, directory, "히브리서", "7:25")
+    add_bible_slide(prs, directory, "고린도전서", "15:6")
+    add_bible_slide(prs, directory, "고린도전서", "15:5", "15:6")
+    add_bible_slide(prs, directory, "사도행전", "4:20")
+    add_bible_slide(prs, directory, "요한일서", "1:1")
+    add_bible_slide(prs, directory, "고린도전서", "15:55")
 
 
     
-    # add_card_slide(prs, input_text= '성찬')
-    # add_hymn_slide(prs, '보혈을 지나')
+    add_card_slide(prs, input_text= '성찬')
+    add_hymn_slide(prs, '나 같은 죄인 살리신')
     add_card_slide(prs, input_text= '통성기도', background_color='000000')
     add_card_slide(prs, input_text= '광고')
-    add_hymn_slide(prs, hymn_list[5])
-    add_hymn_slide(prs,  '부흥 2000')    
+    # add_hymn_slide(prs, hymn_list[5])
+    add_hymn_slide(prs,  '예수 나의 산 소망')    
     add_card_slide(prs, input_text= '축도')
 
     prs.save(F'{today}_늘푸른교회_.pptx')
