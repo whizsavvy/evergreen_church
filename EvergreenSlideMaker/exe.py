7b243a2611cc1878f12a409253e7eb3cf63c10b6
whizsavvy <134269430+whizsavvy@users.noreply.github.com>
2026-05-02 11:14:26 +0900

    Update hymn list and presentation slides

--- EvergreenSlideMaker/exe.py
+++ EvergreenSlideMaker/exe.py
@@ -13,7 +13,7 @@ today = datetime.datetime.now().strftime('%Y-%m-%d')
 
 exec(open("EvergreenSlideMaker/setting.py").read())
 
-hymn_list = ['주 날 위해 버림 받음으로', '빈들에 마른 풀같이', '내 영혼이 은총 입어', '나는 주의 친구']
+hymn_list = ['거친 길 위를 걸어갈 때도', '이 험한 세상 나 살아 갈 동안', '마귀들과 싸울지라', '내가 매일 기쁘게', '나로부터 시작되리']
 
 def create_presentation(hymn_list=[]):
     prs = Presentation()
@@ -26,7 +26,7 @@ def create_presentation(hymn_list=[]):
 
     add_blank_slide(prs)
     add_hymn_slide(prs, hymn_list[0])
-    # add_hymn_slide(prs, hymn_list[1])
+    add_hymn_slide(prs, hymn_list[1])
     # add_hymn_slide(prs, hymn_list[2])
     
 
@@ -35,33 +35,33 @@ def create_presentation(hymn_list=[]):
     add_image_slide(prs, pic_dic+'2026_신앙고백1.JPG')
     add_image_slide(prs, pic_dic+'2026_신앙고백2.JPG')
     # add_card_slide(prs, input_text= '신앙고백', background_color='000000')
-    add_hymn_slide(prs, hymn_list[1])
     add_hymn_slide(prs, hymn_list[2])
     add_hymn_slide(prs, hymn_list[3])
+    add_hymn_slide(prs, hymn_list[4])
     add_blank_slide(prs)
     
 
     # add_card_slide(prs, input_text= '성가대 찬양')
-    # add_bible_slide(prs, directory, "시편", "46:1", "46:11")
-    # add_subtitle_slide(prs, input_text="하나님께 집중하다 – 가만히 있으라 (시편 46:1~11)")
+    add_bible_slide(prs, directory, "사도행전", "1:8")
+    add_subtitle_slide(prs, input_text="성령께서 하시니, 내가 간다 (사도행전 1:8)")
     
-    add_bible_slide(prs, directory, "요한복음", "1:40", "1:42")
-    add_subtitle_slide(prs, input_text="복음은 관계를 통해 흐릅니다 (요한복음 1:40~42)")
-    
-    add_bible_slide(prs, directory, "고린도전서", "1:21")
+    add_bible_slide(prs, directory, "사도행전", "1:8")
+    add_bible_slide(prs, directory, "스가랴", "4:6")
+    add_bible_slide(prs, directory, "요한복음", "6:44")
+    add_bible_slide(prs, directory, "고린도전서", "2:4")
+    add_bible_slide(prs, directory, "요한복음", "9:25")
+    add_bible_slide(prs, directory, "요한복음", "4:35")
+    add_bible_slide(prs, directory, "고린도후서", "6:2")
     add_bible_slide(prs, directory, "로마서", "10:14")
-    add_bible_slide(prs, directory, "요한일서", "1:3")
-    add_bible_slide(prs, directory, "요한복음", "4:28", "4:30")
-    add_bible_slide(prs, directory, "누가복음", "14:23")
-    add_bible_slide(prs, directory, "요한복음", "3:16")
+    add_bible_slide(prs, directory, "누가복음", "11:13")
 
-    add_hymn_slide(prs, '하나님 아버지의 마음')
+    # add_hymn_slide(prs, '하나님 아버지의 마음')
     # add_card_slide(prs, input_text= '성찬')
     # add_hymn_slide(prs, '나 같은 죄인 살리신')
     add_card_slide(prs, input_text= '통성기도', background_color='000000')
     add_card_slide(prs, input_text= '광고')
     # add_hymn_slide(prs, hymn_list[5])
-    add_hymn_slide(prs,  '주 하나님 지으신 모든 세계')    
+    add_hymn_slide(prs,  '그 날')    
     add_card_slide(prs, input_text= '축도')
 
     prs.save(F'{today}_늘푸른교회_.pptx')
