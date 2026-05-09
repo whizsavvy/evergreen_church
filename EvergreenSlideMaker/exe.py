fd8c82bd1cd12742697e62f259a6be43d2f40f3b
whizsavvy <134269430+whizsavvy@users.noreply.github.com>
2026-05-10 04:42:35 +0900

    Update exe.py

--- EvergreenSlideMaker/exe.py
+++ EvergreenSlideMaker/exe.py
@@ -13,7 +13,7 @@ today = datetime.datetime.now().strftime('%Y-%m-%d')
 
 exec(open("EvergreenSlideMaker/setting.py").read())
 
-hymn_list = ['거친 길 위를 걸어갈 때도', '이 험한 세상 나 살아 갈 동안', '마귀들과 싸울지라', '내가 매일 기쁘게', '나로부터 시작되리']
+hymn_list = ['송축해 내 영혼', '슬픈 마음 있는 사람', '크신 내 주님', '예수 열방의 소망', '비 준비하시니']
 
 def create_presentation(hymn_list=[]):
     prs = Presentation()
@@ -42,20 +42,27 @@ def create_presentation(hymn_list=[]):
     
 
     # add_card_slide(prs, input_text= '성가대 찬양')
-    add_bible_slide(prs, directory, "사도행전", "1:8")
-    add_subtitle_slide(prs, input_text="성령께서 하시니, 내가 간다 (사도행전 1:8)")
+    add_bible_slide(prs, directory, "고린도전서", "3:6", "3:7")
+    add_subtitle_slide(prs, input_text="자라게 하시는 하나님 (고린도전서 3:6~7)")
     
     add_bible_slide(prs, directory, "사도행전", "1:8")
-    add_bible_slide(prs, directory, "스가랴", "4:6")
-    add_bible_slide(prs, directory, "요한복음", "6:44")
-    add_bible_slide(prs, directory, "고린도전서", "2:4")
-    add_bible_slide(prs, directory, "요한복음", "9:25")
-    add_bible_slide(prs, directory, "요한복음", "4:35")
     add_bible_slide(prs, directory, "고린도후서", "6:2")
-    add_bible_slide(prs, directory, "로마서", "10:14")
-    add_bible_slide(prs, directory, "누가복음", "11:13")
+    add_bible_slide(prs, directory, "요한복음", "6:44")
+    add_bible_slide(prs, directory, "사도행전", "16:31")
+    add_bible_slide(prs, directory, "사도행전", "8:35")
+    add_bible_slide(prs, directory, "누가복음", "14:23")
+    add_bible_slide(prs, directory, "로마서", "11:25")
+    add_bible_slide(prs, directory, "마태복음", "28:19", "28:20")
+    add_bible_slide(prs, directory, "요한복음", "3:16")
+    add_bible_slide(prs, directory, "고린도후서", "5:14")
+    add_bible_slide(prs, directory, "고린도전서", "9:19")
+    add_bible_slide(prs, directory, "고린도전서", "9:22")
+    add_bible_slide(prs, directory, "로마서", "1:16")
+    add_bible_slide(prs, directory, "열왕기하", "5:3")
+    add_bible_slide(prs, directory, "열왕기하", "5:17")
+    add_bible_slide(prs, directory, "디모데후서", "4:2")
 
-    # add_hymn_slide(prs, '하나님 아버지의 마음')
+    add_hymn_slide(prs, '부름 받아 나선 이 몸')
     # add_card_slide(prs, input_text= '성찬')
     # add_hymn_slide(prs, '나 같은 죄인 살리신')
     add_card_slide(prs, input_text= '통성기도', background_color='000000')
