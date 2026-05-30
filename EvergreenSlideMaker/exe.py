28cf9d7667e9b460029c0bcdc783e972a5a7184e
whizsavvy <134269430+whizsavvy@users.noreply.github.com>
2026-05-31 03:46:07 +0900

    Update hymn list and Bible verses in presentation

--- EvergreenSlideMaker/exe.py
+++ EvergreenSlideMaker/exe.py
@@ -13,7 +13,7 @@ today = datetime.datetime.now().strftime('%Y-%m-%d')
 
 exec(open("EvergreenSlideMaker/setting.py").read())
 
-hymn_list = ['세상의 유혹 시험이', '슬픈 마음 있는 사람', '내 영혼이 은총 입어', '태산을 넘어 험곡에 가도', '크신 내 주님']
+hymn_list = ['당신은 사랑 받기 위해', '저 바다보다도 더 넓고', '나의 가장 낮은 마음', '꽃들도']
 
 def create_presentation(hymn_list=[]):
     prs = Presentation()
@@ -37,24 +37,18 @@ def create_presentation(hymn_list=[]):
     # add_card_slide(prs, input_text= '신앙고백', background_color='000000')
     add_hymn_slide(prs, hymn_list[2])
     add_hymn_slide(prs, hymn_list[3])
-    add_hymn_slide(prs, hymn_list[4])
+    # add_hymn_slide(prs, hymn_list[4])
     # add_hymn_slide(prs, hymn_list[6])
     add_blank_slide(prs)
     
 
-    add_bible_slide(prs, directory, "고린도후서", "2:15")
-    add_subtitle_slide(prs, input_text="당신은 그리스도의 향기입니다 (고린도후서 2:15)")
+    add_bible_slide(prs, directory, "마태복음", "6:28", "6:30")
+    add_subtitle_slide(prs, input_text="꽃처럼 피어나는 인생 (마태복음 6:28~30)")
     
-    add_bible_slide(prs, directory, "고린도전서", "1:21")
-    add_bible_slide(prs, directory, "고린도후서", "2:15")
-    add_bible_slide(prs, directory, "고린도후서", "3:3")
-    add_bible_slide(prs, directory, "로마서", "9:2")
-    add_bible_slide(prs, directory, "로마서", "9:3")
-    add_bible_slide(prs, directory, "고린도후서", "5:14")
-    add_bible_slide(prs, directory, "누가복음", "15:4")
-    add_bible_slide(prs, directory, "마태복음", "16:26")
-    add_bible_slide(prs, directory, "요한복음", "1:45")
-    add_bible_slide(prs, directory, "사도행전", "1:8")
+    add_bible_slide(prs, directory, "마태복음", "6:30")
+    add_bible_slide(prs, directory, "요한복음", "3:16")
+    add_bible_slide(prs, directory, "로마서", "5:8")
+    add_bible_slide(prs, directory, "고린도후서", "5:17")
 
     # add_hymn_slide(prs, '부름 받아 나선 이 몸')
     # add_card_slide(prs, input_text= '성찬')
