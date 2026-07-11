a9da25b984104437a461973cbad0e890a4a5d2d5
whizsavvy <134269430+whizsavvy@users.noreply.github.com>
2026-07-11 23:02:18 +0900

    Update exe.py

--- EvergreenSlideMaker/exe.py
+++ EvergreenSlideMaker/exe.py
@@ -13,7 +13,7 @@ today = datetime.datetime.now().strftime('%Y-%m-%d')
 
 exec(open("EvergreenSlideMaker/setting.py").read())
 
-hymn_list = ['송축해 내 영혼', '주 예수 이름 높이어', "주님의 은혜 내게 넘치네", "생수의 강", "비 준비하시니", "나의 영혼이 잠잠히"]
+hymn_list = ['우리 보좌 앞에 모였네', '피난처 되시는 주 예수', '죄에서 자유를 얻게 함은', '예수 열방의 소망', '하나님의 나라 온 땅 흔드네', '하나님의 부르심', '주님 다시 오실 때 까지' , '파송의 노래']
 
 def create_presentation(hymn_list=[]):
     prs = Presentation()
@@ -41,17 +41,21 @@ def create_presentation(hymn_list=[]):
     add_hymn_slide(prs, hymn_list[5])
     add_blank_slide(prs)
     
+
+    add_hymn_slide(prs, hymn_list[6])
+    add_bible_slide(prs, directory, "마태복음", "6:25", "6:33")
+    add_subtitle_slide(prs, input_text="먼저 그 나라와 의를 구하는 사람 (마태복음 6:25~33)")
     
-    add_bible_slide(prs, directory, "마태복음", "14:22", "14:33")
-    add_subtitle_slide(prs, input_text="위기를 기회로 (마태복음 14:22~33)")
-    
-    add_bible_slide(prs, directory, "마태복음", "14:24")
-    add_bible_slide(prs, directory, "시편", "84:6")
-    add_bible_slide(prs, directory, "마태복음", "14:25")
-    add_bible_slide(prs, directory, "마가복음", "6:48")
-    add_bible_slide(prs, directory, "시편", "121:4", "121:5")
-    add_bible_slide(prs, directory, "사도행전", "1:8")
-    add_bible_slide(prs, directory, "마태복음", "14:22")
+    add_bible_slide(prs, directory, "창세기", "12:1")
+    add_bible_slide(prs, directory, "열왕기상", "3:11")
+    add_bible_slide(prs, directory, "빌립보서", "4:19")
+    add_bible_slide(prs, directory, "마태복음", "6:25")
+    add_bible_slide(prs, directory, "마태복음", "6:26")
+    add_bible_slide(prs, directory, "마태복음", "6:28")
+    add_bible_slide(prs, directory, "로마서", "10:14")
+    add_bible_slide(prs, directory, "요한복음", "20:21")
+    add_bible_slide(prs, directory, "마태복음", "6:33")
+    add_bible_slide(prs, directory, "빌립보서", "4:19")
 
     # add_hymn_slide(prs, hymn_list[4])
 
@@ -60,8 +64,10 @@ def create_presentation(hymn_list=[]):
     # add_hymn_slide(prs, '나 같은 죄인 살리신')
     add_card_slide(prs, input_text= '통성기도')
     add_card_slide(prs, input_text= '광고')
-    # add_hymn_slide(prs, hymn_list[5])
-    add_hymn_slide(prs,  '부흥 2000')    
+    add_card_slide(prs, input_text= '파송기도 및 축복')
+    add_hymn_slide(prs, hymn_list[7])
+    # add_hymn_slide(prs,  '부흥 2000')
+    
     add_card_slide(prs, input_text= '축도')
 
     prs.save(F'{today}_늘푸른교회_.pptx')
